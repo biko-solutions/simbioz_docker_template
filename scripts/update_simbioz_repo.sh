5fb99f8d33560e0d9896e29f62410b8a8f4d43d7
Borovlev A. S <bas@qdes.com.ua>
2024-11-06 02:45:52 +0200

    chore: Update script to use bash instead of sh for database update

--- scripts/update_simbioz_repo.sh
+++ scripts/update_simbioz_repo.sh
@@ -4,4 +4,4 @@ git submodule foreach --recursive git reset --hard
 git submodule foreach --recursive 'git fetch origin --tags && git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo "main") && git pull'
 sh odoo_set_icons.sh
 
-sh ../scripts/update_database.sh
+bash ../scripts/update_database.sh
